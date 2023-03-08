d804057c86a96b7a4f075771e005c9c547d0123c
DEV <DEV@DEV.COM>
2023-03-08 18:53:38 +0500

    AddressPoints Fix

--- src/views/private/Admin/Map/Map.jsx
+++ src/views/private/Admin/Map/Map.jsx
@@ -11,6 +11,7 @@ import DataTiles, { visibility } from './DataTiles';
 import { Boundary } from '../Dashboard/Submap';
 import Gpx from './Gpx';
 import AddressPoints from './AddressPoints'
+import InfoCard from './InfoCard';
 
 export const mapStyle = signal('https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json')
 
