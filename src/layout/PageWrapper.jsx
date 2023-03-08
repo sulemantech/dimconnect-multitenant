82efe0b640d8f9221681538a56598bf6bb672110
DEV <DEV@DEV.COM>
2023-03-08 19:16:03 +0500

    Errors Fix

--- src/layout/PageWrapper.jsx
+++ src/layout/PageWrapper.jsx
@@ -1,11 +1,11 @@
 import Header from "./Header"
 
 export default ({ children }) => {
-    return(
+    return (
         <div className="relative flex flex-col flex-1 w-full">
             <Header />
             {children}
-            
+
         </div>
     )
 }
