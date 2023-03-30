4b1f5127ad77e0310c698af1ea00cfda7f92579d
DEV <DEV@DEV.COM>
2023-03-30 15:26:31 +0500

    token param

--- src/views/public/WebView/ProtectedWrapper.jsx
+++ src/views/public/WebView/ProtectedWrapper.jsx
@@ -1,21 +1,25 @@
+import { LoadingOverlay } from "@mantine/core"
 import { useLayoutEffect, useState } from "preact/hooks"
+import api from "../../../api"
 
 export default ({children}) => {
 
-    const [isAuthenticated, setIsAuthenticated] = useState(true)
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
 
     useLayoutEffect(() => {
         const queryParam = new URLSearchParams(window.location.search)
         const username = queryParam.get('username')
         const password = queryParam.get('password')
-        if (username === 'hiwifi' && password === 'admin') {
+        const token = queryParam.get('token')
+        if (username === 'hiwifi' && password === 'admin' && token) {
             setIsAuthenticated(true)
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`
         }
     }, [])
 
     return (
         <>
-            {isAuthenticated ? children : <div>Not Authenticated</div>}
+            {isAuthenticated ? children : <div><LoadingOverlay visible/></div>}
         </>
     )
 }
