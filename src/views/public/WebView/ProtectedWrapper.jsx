b919395d04a82a116eb0a98fe68677e79bd9e055
DEV <DEV@DEV.COM>
2023-05-30 14:24:40 +0500

    Region List From Api

--- src/views/public/WebView/ProtectedWrapper.jsx
+++ src/views/public/WebView/ProtectedWrapper.jsx
@@ -1,27 +1,39 @@
 import { LoadingOverlay } from "@mantine/core"
 import { useEffect, useState } from "preact/hooks"
-import api from "../../../api"
+import api, { getCurrentUserPermissions } from "../../../api"
 import appConfig from "../../../config/appConfig"
+import { permissible } from "../../../signals"
 
-export default ({children}) => {
+export default ({ children }) => {
 
-    
-    
+
     const queryParam = new URLSearchParams(window.location.search)
     const token = queryParam.get('token')
     if (token) {
-        
+
         api.defaults.headers.common['authorization'] = `Bearer ${token}`
         sessionStorage.setItem(appConfig.sessionStorageKeyWebview, token)
-    
+
+        getCurrentUserPermissions().then(({ data }) => {
+            permissible.value = permissible.value.map( p => {
+                const index = data.accessList.findIndex(a => a.activity === p.activity)
+                if (index !== -1) {
+                    return data.accessList[index]
+                }
+                return p
+            } )
+        }).catch(err => {
+
+        })
+
     }
-    
+
     const [isAuthenticated, setIsAuthenticated] = useState(token ? true : false)
-  
+
 
     return (
         <>
-            {isAuthenticated ? children : <div><LoadingOverlay visible/></div>}
+            {isAuthenticated ? children : <div><LoadingOverlay visible /></div>}
         </>
     )
 }
