f3093b82d51afe816ee56d3f26d365e9932dfc49
DEV <DEV@DEV.COM>
2023-08-03 01:27:28 -0400

    Mozilla Support Added

---
+++ src/views/private/Admin/Map/MuncipalitiesLayer.jsx
@@ -0,0 +1,8 @@
+// https://www.geoportal.rlp.de/mapbender/php/wms.php?layer_id=61680&withChilds=1&REQUEST=GetCapabilities&VERSION=1.1.1&SERVICE=WMS
+import { Source,Layer } from "react-map-gl"
+export default () => {
+return(
+    <></>
+)
+}
+        
